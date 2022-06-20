4c7e3a2bbf5b9e446dd879207ae37595db3098dc
Doniyor333 <Yulaynboy@mail.ru>
2022-06-20 21:02:23 +0500

    ozgarishlar boldi

---
+++ Exam_Banana/HomeVC.swift
@@ -0,0 +1,19 @@
+//
+//  HomeVC.swift
+//  Exam_Banana
+//
+//  Created by DonnY Isayev on 20/06/22.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+
+}
